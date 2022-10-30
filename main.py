b2ad8a56f0ffe2f09a26c80da93b1036233d029e
johnFromE <89530839+johnFromE@users.noreply.github.com>
2022-10-30 13:17:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,103 @@
-import telebot
-import sys
+from pyrogram import Client,filters
+from random_movie import choose_movie
 from filter import fl
 
-from_id = "-1001890974134"
-my_id = "432175862"
+bot = Client("amhric_bot", api_id=20453853, api_hash="fe2593c973640679de717af61e1f805c",bot_token="5737216854:AAEzEb7If0vA_zpLlKxq5vIujMjK6nEfSD0")
 
-Token = '5737216854:AAEzEb7If0vA_zpLlKxq5vIujMjK6nEfSD0'
-bot = telebot.TeleBot(Token)
+@bot.on_message(filters.document)
+def upload_doc(bot, m):
+    s_movie_name = str(m.document.file_name) +" "+ str(m.caption)
+    s_movie_name = fl.filter_word(s_movie_name)
 
-try:
+    file = open("file.txt", 'a', encoding="utf-8")
+    file.write("\n" + str(m.id) + ":" + s_movie_name)
+    file.close()
 
-	@bot.message_handler(commands=["start"])
-	def start(msg):
-		bot.send_message(msg.chat.id,"ሰላም "+msg.chat.first_name+"\nእዚህ ሁሉንም የአማርኛ ፊልሞች ማግኘት ትችላላችሁ\nሰርች ለማድረግ /search ያስቀድሙ\nለምሳሌ:- /search አብሳላት\n\nጥያቄ ካላቹ @edit_jo ")
-		contct_file = open("contacte.txt","a",encoding="utf-8")
-		contct_file.write("\n"+str(msg.chat.id))
-		contct_file.close()
+@bot.on_message(filters.video)
+def upload_vid(bot,m):
+    s_movie_name = str(m.video.file_name) + " " + str(m.caption)
+    s_movie_name = fl.filter_word(s_movie_name)
 
-	@bot.message_handler(commands=["its_jo_send_doc"])
-	def dad(messgae):
-		file_dad = open("file.txt","rb")
-		bot.send_document(messgae.chat.id,file_dad)
-		file_dad.close()
-		file_dad_two = open("contacte.txt", "rb")
-		bot.send_document(messgae.chat.id, file_dad_two)
-		file_dad_two.close()
-		print("file sent to dad!")
+    file = open("file.txt", 'a', encoding="utf-8")
+    file.write("\n" + str(m.id) + ":" + s_movie_name)
+    file.close()
 
-	@bot.channel_post_handler(content_types=['document'])
-	def upload(m):
-		s_movie_name = str(m.document.file_name) +" "+ str(m.caption)
-		s_movie_name = fl.filter_word(s_movie_name)
+@bot.on_message(filters.command('search'))
+def command(bot,message):
+    if message.text == "/search":
+        bot.send_message(message.chat.id, "በስትክክል አልገባም\nሰርች ለማድረግ /search ያስቀድሙ\nለምሳሌ:- /search የወንዶች ጉዳይ")
+    else:
+        movie_name = str(message.text).replace("/search", "")
+        movie_name = movie_name[1:]
 
-		file = open("file.txt", 'a', encoding="utf-8")
-		file.write("\n" + str(m.message_id) + ":" + s_movie_name)
-		file.close()
+        r_file = open("file.txt", "r", encoding="utf-8")
+        word_list = r_file.read().split("\n")
+        found = False
 
-	@bot.channel_post_handler(content_types=['video'])
-	def upload(m):
-		s_movie_name = str(m.video.file_name) +" "+ str(m.caption)
-		s_movie_name = fl.filter_word(s_movie_name)
+        for w in word_list:
+            word_splitd = w.split(":")
+            movie_id = word_splitd[0]
+            movie_title = word_splitd[1].split(" ")
+            bool = []
+            for mn in movie_name.split(" "):
+                if mn in movie_title:
+                    bool.append("true")
+                else:
+                    bool.append("false")
 
-		file = open("file.txt", 'a', encoding="utf-8")
-		file.write("\n"+str(m.message_id)+":"+s_movie_name)
-		file.close()
+            if "false" in bool:
+                found = False
+            else:
+                print(bool)
+                found = True
+                # bot.forward_message(chat_id=m.chat.id, from_chat_id=from_id, message_id=int(movie_id))
+                msg = bot.get_messages(-1001890974134, int(movie_id))
+                msg.copy(  # copy() so there's no "forwarded from" header
+                    chat_id=message.chat.id,  # the channel you want to post to
+                    caption="Amharic Film Bot\n\nhttps://t.me/amharic_film_bot\nhttps://t.me/amharic_film_bot"
+                    # mentions the posting user in the new message
+                )
+                break
 
-	@bot.message_handler(commands=['search'])
-	def search(m):
-		if m.text == "/search":
-			bot.send_message(m.chat.id,"በስትክክል አልገባም\nሰርች ለማድረግ /search ያስቀድሙ\nለምሳሌ:- /search የወንዶች ጉዳይ")
-		else:
-			movie_name = str(m.text).replace("/search","")
-			movie_name = movie_name[1:]
+        if found == False:
+            bot.send_message(message.chat.id, movie_name + " ማገኘት አልተቻለም \nወይም ስሙን በስትክክል አስገብተው ደግመው ይሞክር")
 
-			r_file = open("file.txt","r",encoding="utf-8")
-			word_list = r_file.read().split("\n")
-			found = False
+@bot.on_message(filters.command('start'))
+def start(bot,msg):
+    bot.send_message(msg.chat.id,"ሰላም " + msg.chat.first_name + "\nእዚህ ሁሉንም የአማርኛ ፊልሞች ማግኘት ትችላላችሁ\nሰርች ለማድረግ /search ያስቀድሙ\nለምሳሌ:- /search አብሳላት\n\nጥያቄ ካላቹ @edit_jo ")
 
-			for w in word_list:
-				word_splitd = w.split(":")
-				movie_id = word_splitd[0]
-				movie_title = word_splitd[1].split(" ")
-				bool = []
-				for mn in movie_name.split(" "):
-					if mn in movie_title:
-						bool.append("true")
-					else:
-						bool.append("false")
+    #save user info
+    read_contacte = open("contacte.txt", "r", encoding="utf-8")
+    read = read_contacte.read().split("\n")
+    new_user = True
+    for rc in read:
+        rc_split = rc.split(":")
+        user_id = rc_split[1]
+        if int(user_id) == msg.chat.id:
+            new_user = False
+            break
+    read_contacte.close()
+    if new_user == True:
+        open("contacte.txt", "a", encoding="utf-8").write("\n"+str(len(read)+1)+"."+msg.from_user.first_name+":"+str(msg.chat.id))
+@bot.on_message(filters.command('random'))
+def random(bot,msg):
+    movie_id = choose_movie()
+    message = bot.get_messages(-1001890974134, int(movie_id))
+    message.copy(  # copy() so there's no "forwarded from" header
+        chat_id=msg.chat.id,  # the channel you want to post to
+        caption="Amharic Film Bot\n\nhttps://t.me/amharic_film_bot\nhttps://t.me/amharic_film_bot"
+        # mentions the posting user in the new message
+    )
 
-				if "false" in bool:
-					found=False
-				else:
-					print(bool)
-					found=True
-					#bot.send_document(chat_id=m.chat.id, from_chat_id=from_id, message_id=int(movie_id))
-					bot.forward_message(chat_id=m.chat.id, from_chat_id=from_id, message_id=int(movie_id))
-					break
-
-
-			if found==False:
-				bot.send_message(m.chat.id,movie_name+" ማገኘት አልተቻለም \nወይም ስሙን በስትክክል አስገብተው ደግመው ይሞክር")
-
-
-except:
-	bot.send_message(my_id, "Oops! "+sys.exc_info()[0])
-print("bot running...")
-bot.polling()
-bot.send_message(my_id,"hy jo the bot stoped running!")
+@bot.on_message(filters.command('send_doc'))
+def dad(bot,messgae):
+    file_dad = open("file.txt", "rb")
+    bot.send_document(messgae.chat.id, file_dad)
+    file_dad.close()
+    file_dad_two = open("contacte.txt", "rb")
+    bot.send_document(messgae.chat.id, file_dad_two)
+    file_dad_two.close()
+@bot.on_message()
+def message(bot,msg):
+    bot.send_message(msg.chat.id,msg.text+" የማይታወቅ ኮማንድ")
+bot.run()
